d4d76343cc834e02d041e63eb05a118a46569b79
Rahul <97379229+Rahulrah1997@users.noreply.github.com>
2025-12-16 18:29:59 +0530

    Update CricketDashboard.py

--- env/CricketDashboard.py
+++ env/CricketDashboard.py
@@ -4,6 +4,8 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import altair as alt
+import time
+import json
 
 matches = []
 
@@ -11,16 +13,21 @@ st.sidebar.title("Criket Dashboard")
 #st.sidebar.header("Select a page to view details")
 Pageselection =st.sidebar.selectbox("Select Match",['Live Matches','Player Stats','SQL Analytics','CRUD Operations'])
 
+#@st.fragment(run_every=5)
 def livematches():
 
-    url = "https://cricbuzz-cricket.p.rapidapi.com/matches/v1/live"
+    # url = "https://cricbuzz-cricket.p.rapidapi.com/matches/v1/live"
 
-    headers = {
-	"x-rapidapi-key": "a5528101fdmshd5d19c93da4909ep189b28jsnc2030d0871ae",
-	"x-rapidapi-host": "cricbuzz-cricket.p.rapidapi.com"
-                }
-    response = requests.get(url, headers=headers)
-    data = response.json()
+    # headers = {
+	# "x-rapidapi-key": "a5528101fdmshd5d19c93da4909ep189b28jsnc2030d0871ae",
+    # "x-rapidapi-key": "e19f088f4emsh5939c6cc237aa1fp175797jsne25dc4cf301b",
+	# "x-rapidapi-host": "cricbuzz-cricket.p.rapidapi.com"
+    #             }
+    # response = requests.get(url, headers=headers)
+    # data = response.json()
+
+    with open ('matchesdata.json','r',encoding='utf-8') as file:
+     data = json.load(file)
 
     
 
@@ -37,11 +44,13 @@ def livematches():
                 match_format = info.get("matchFormat")
                 start_date = info.get("startDate")
                 match_status = info.get("status")
+                state = info.get("state")
                 team1_name = info.get("team1",{}).get("teamName")
                 team1_sname = info.get("team1",{}).get("teamSName")
                 team2_name = info.get("team2",{}).get("teamName")
                 team2_sname = info.get("team2",{}).get("teamSName")
                 Ground_name = info.get("venueInfo",{}).get("ground")
+                city = info.get("venueInfo",{}).get("city")
 
                 matches.append({
                     'series_id': series_id,
@@ -54,8 +63,10 @@ def livematches():
                     'team1_name': team1_name,
                     'team1_sname': team1_sname,
                     'team2_name': team2_name,
-                    'team1_sname': team1_sname,
-                    'Ground_name': Ground_name
+                    'team2_sname': team2_sname,
+                    'Ground_name': Ground_name,
+                    'city': city,
+                    'state':state
             }
 
             )
@@ -67,9 +78,35 @@ def livematches():
 if Pageselection == 'Live Matches':
 
     livematches()
-    options = [m['series_name']for m in matches]
-    st.title('Live Match Dashboard',width='stretch')
-    st.selectbox('Select a Match',options)
+    st.title("Live Match Dashboard",width='stretch')
+    selected_match = st.selectbox('Select a Match',options=matches,
+                 format_func=lambda m: f"{m['series_name']} ({m['team1_sname']} vs {m['team2_sname']})")
+    
+    if selected_match:
+        formatter = lambda n:f"Match Details: ({n['team1_name']} vs {n['team2_name']})"
+        subheader_text = formatter(selected_match)
+        st.subheader(subheader_text)
+
+        description = selected_match['description']
+        Match_Format = selected_match['match_format']
+        Venue = selected_match['Ground_name']
+        City = selected_match['city']
+        Status = selected_match['match_status']
+        State = selected_match['state']
+
+        c1,c2 = st.columns(2)
+
+
+        with c1:
+            st.markdown(f"Match: {description}")
+            st.markdown(f"Format: {Match_Format}")
+            st.markdown(f"Venue: {Venue}")
+
+        with c2:
+            st.markdown(f"City: {City}")
+            st.markdown(f"Status: {Status}")
+            st.markdown(f"State: {State}")
+
 
 
    
