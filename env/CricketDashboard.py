23369f1cfc0990761896f085b8da5645baba0d40
Rahul <97379229+Rahulrah1997@users.noreply.github.com>
2025-12-17 18:36:01 +0530

    Update CricketDashboard.py

--- env/CricketDashboard.py
+++ env/CricketDashboard.py
@@ -52,12 +52,18 @@ def livematches():
                 Ground_name = info.get("venueInfo",{}).get("ground")
                 city = info.get("venueInfo",{}).get("city")
                 matchScore = match.get('matchScore',{})
-                team1score = matchScore.get('team1Score',{}).get('inngs1',{}).get('runs')
-                team1wicket = matchScore.get('team1Score',{}).get('inngs1',{}).get('wickets')
-                team1overs = matchScore.get('team1Score',{}).get('inngs1',{}).get('overs')
-                team2score = matchScore.get('team2Score',{}).get('inngs1',{}).get('runs')
-                team2wicket = matchScore.get('team2Score',{}).get('inngs1',{}).get('wickets')
-                team2overs = matchScore.get('team2Score',{}).get('inngs1',{}).get('overs')
+                team1score_inn1 = matchScore.get('team1Score',{}).get('inngs1',{}).get('runs')
+                team1wicket_inn1 = matchScore.get('team1Score',{}).get('inngs1',{}).get('wickets')
+                team1overs_inn1 = matchScore.get('team1Score',{}).get('inngs1',{}).get('overs')
+                team2score_inn1 = matchScore.get('team2Score',{}).get('inngs1',{}).get('runs')
+                team2wicket_inn1 = matchScore.get('team2Score',{}).get('inngs1',{}).get('wickets')
+                team2overs_inn1 = matchScore.get('team2Score',{}).get('inngs1',{}).get('overs')
+                team1score_inn2 = matchScore.get('team1Score',{}).get('inngs2',{}).get('runs')
+                team1wicket_inn2 = matchScore.get('team1Score',{}).get('inngs2',{}).get('wickets')
+                team1overs_inn2 = matchScore.get('team1Score',{}).get('inngs2',{}).get('overs')
+                team2score_inn2 = matchScore.get('team2Score',{}).get('inngs2',{}).get('runs')
+                team2wicket_inn2 = matchScore.get('team2Score',{}).get('inngs2',{}).get('wickets')
+                team2overs_inn2 = matchScore.get('team2Score',{}).get('inngs2',{}).get('overs')
 
                 matches.append({
                     'series_id': series_id,
@@ -74,12 +80,18 @@ def livematches():
                     'Ground_name': Ground_name,
                     'city': city,
                     'state':state,
-                    'team1score':team1score,
-                    'team1wicket':team1wicket,
-                    'team1overs':team1overs,
-                    'team2score':team2score,
-                    'team2wicket':team2wicket,
-                    'team2overs':team2overs
+                    'team1score_inn1':team1score_inn1,
+                    'team1wicket_inn1':team1wicket_inn1,
+                    'team1overs_inn1':team1overs_inn1,
+                    'team2score_inn1':team2score_inn1,
+                    'team2wicket_inn1':team2wicket_inn1,
+                    'team2overs_inn1':team2overs_inn1,
+                    'team1score_inn2':team1score_inn2,
+                    'team1wicket_inn2':team1wicket_inn2,
+                    'team1overs_inn2':team1overs_inn2,
+                    'team2score_inn2':team2score_inn2,
+                    'team2wicket_inn2':team2wicket_inn2,
+                    'team2overs_inn2':team2overs_inn2
             }
 
             )
@@ -91,38 +103,66 @@ def livematches():
 if Pageselection == 'Live Matches':
 
     livematches()
-    st.title("Live Match Dashboard",width='stretch')
+    st.title("Live Match Dashboard 🏏",width='stretch')
     selected_match = st.selectbox('Select a Match',options=matches,
                  format_func=lambda m: f"{m['series_name']} ({m['team1_sname']} vs {m['team2_sname']})")
+    
+    st.write(selected_match['team1overs_inn1'])
     
     if selected_match:
         formatter = lambda n:f"Match Details: ({n['team1_name']} vs {n['team2_name']})"
         subheader_text = formatter(selected_match)
         st.subheader(subheader_text)
+        st.info(f"***{selected_match['series_name']}***")
 
-        description = selected_match['description']
-        Match_Format = selected_match['match_format']
-        Venue = selected_match['Ground_name']
-        City = selected_match['city']
-        Status = selected_match['match_status']
-        State = selected_match['state']
-        Team1_Score = selected_match['team1score']
-        Team2_Score = selected_match['team2score']
+        MatchDetails = {'Description':selected_match['description'],
+                        'Match_Format':selected_match['match_format'],
+                        'Venue':selected_match['Ground_name'],
+                        'City':selected_match['city'],
+                        'Status':selected_match['match_status'],
+                        'State':selected_match['state']}
+        
+        
+        Team1details_inn1 = {'Score':selected_match['team1score_inn1'],'Overs':selected_match['team1overs_inn1'],
+                       'wicket':selected_match['team1wicket_inn1'],'TeamName':selected_match['team1_name']}
+        
+        if selected_match['team1score_inn2']:
+            Team1details_inn2 = {'Score':selected_match['team1score_inn2'],'Overs':selected_match['team1overs_inn2'],
+                        'wicket':selected_match['team1wicket_inn2'],'TeamName':selected_match['team1_name']}
+        
+        Team2details_inn1 = {'Score':selected_match['team2score_inn1'],'overs':selected_match['team2overs_inn1'],
+                       'wicket':selected_match['team2wicket_inn1'],'TeamName':selected_match['team2_name']}
+        
+        if selected_match['team2score_inn2']:
+            Team2details_inn2 = {'Score':selected_match['team2score_inn2'],'overs':selected_match['team2overs_inn2'],
+                            'wicket':selected_match['team2wicket_inn2'],'TeamName':selected_match['team2_name']}
+        
+        st.markdown(f"*{MatchDetails['Description']}* | {MatchDetails['Match_Format']}")
+        st.markdown(f"*{MatchDetails['Venue']}* | {MatchDetails['City']}")
+        st.markdown(f"*{MatchDetails['Status']}* | {MatchDetails['State']}")
+
+        st.markdown("---")
 
         c1,c2 = st.columns(2)
 
-
         with c1:
-            st.markdown(f"Match: {description}")
-            st.markdown(f"Format: {Match_Format}")
-            st.markdown(f"Venue: {Venue}")
-            st.markdown(Team1_Score)
+            st.markdown(f"**{Team1details_inn1['TeamName']}**")
+            st.metric(label="Score", value=Team1details_inn1['Score'])
+            c1_inner1, c1_inner2 = st.columns(2)
+            c1_inner1.metric("Overs", Team1details_inn1['overs'])
+            c1_inner2.metric("Wickets", Team1details_inn1['wicket'])
+            st.markdown("---")
+
 
         with c2:
-            st.markdown(f"City: {City}")
-            st.markdown(f"Status: {Status}")
-            st.markdown(f"State: {State}")
-            st.markdown(Team2_Score)
+            st.markdown(f"**{Team2details_inn1['TeamName']}**")
+            st.metric(label="Score", value=Team2details_inn1['Score'])
+            c2_inner1, c2_inner2 = st.columns(2)
+            c2_inner1.metric("Overs", Team2details_inn1['overs'])
+            c2_inner2.metric("Wickets", Team2details_inn1['wicket'])
+            st.markdown("---")
+
+
 
 
 
