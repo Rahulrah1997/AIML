b0eecf2c1075feac338aa7c598976e5ad6792e28
Rahul <rajamanickam.rahul@gmail.com>
2025-12-28 01:06:32 +0530

    new changes

--- env/CricketDashboard.py
+++ env/CricketDashboard.py
@@ -1,15 +1,8 @@
-import requests
 import streamlit as st
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import altair as alt
-import time
-import json
 from LiveDashbord import Live_Dashbord 
 from PlayerStats import Player_Stats
 
-#LD = Live_Dashbord()
+LD = Live_Dashbord()
 ps = Player_Stats()
 st.sidebar.title("Criket Dashboard")
 #st.sidebar.header("Select a page to view details")
@@ -17,9 +10,9 @@ Pageselection =st.sidebar.selectbox("Select Match",['Live Matches','Player Stats
 
 #st.write(matches)
 
-# if Pageselection == 'Live Matches':
+if Pageselection == 'Live Matches':
 
-#     LD.live_matches()
+    LD.live_matches()
 
 if Pageselection == 'Player Stats':
 
